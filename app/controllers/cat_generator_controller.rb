effaf0d16da23e050c189f82fe9fc1d103765a85
dannyyu92 <dannyyu92@server.fake>
2013-11-03 01:49:52 -0400

    Add cat parser to controller

--- app/controllers/cat_generator_controller.rb
+++ app/controllers/cat_generator_controller.rb
@@ -1,4 +1,11 @@
 class CatGeneratorController < ApplicationController
+	require 'nokogiri'
+	require 'open-uri'
+
 	def index
+		url = "http://www.asciiworld.com/-Cats-.html"
+		doc = Nokogiri::HTML(open(url))
+		cats = Array(doc.css("pre"))
+		@cat = cats.sample
 	end
 end
